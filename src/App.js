0aabee5060fd4d17a463a963a3db7a0445f454e8
Emerson Boyd <emerson.boyd@yahoo.com>
2022-08-15 19:38:44 -0500

    first commit! <3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import MyButton from "./MyButton"
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Router>
+        <MyButton/>
+      </Router>
+    </main>
   );
 }
 
---
+++ src/MyButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+export default function MyButton() {
+    return (
+        <button className=
+            "text-blue-400 bg-gray-90 body-font">Test</button>
+    )
+};
