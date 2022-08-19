00a15ba414ed6a62738e000ea92674b25a52cbb6
corylennox <corylennox95@gmail.com>
2022-08-19 19:12:24 -0400

    lil bitta dis n a lil bitta dat

--- src/Proposition.jsx
+++ src/Proposition.jsx
@@ -8,11 +8,11 @@ export default class Proposition extends Component {
                 <h2 className='text-white text-xl'>NBA Championship</h2>
                 <div class="grid grid-cols-3 mb-4 border-2 border-green-600">
 
-                    <div class="mb-4 bg-gray-400 h-12"><TeamLine/></div>
-                    <div class="mb-4 bg-gray-500 h-12"><TeamLine/></div>
-                    <div class="mb-4 bg-gray-400 h-12"><TeamLine/></div>
-                    <div class="mb-4 bg-gray-500 h-12"><TeamLine/></div>
-                    <div class="mb-4 bg-gray-400 h-12"><TeamLine/></div>
+                    <div class="mb-4 bg-gray-400 h-12"><TeamLine /></div>
+                    <div class="mb-4 bg-gray-500 h-12"><TeamLine /></div>
+                    <div class="mb-4 bg-gray-400 h-12"><TeamLine /></div>
+                    <div class="mb-4 bg-gray-500 h-12"><TeamLine /></div>
+                    <div class="mb-4 bg-gray-400 h-12"><TeamLine /></div>
                 </div>
 
 
--- src/TeamLine.jsx
+++ src/TeamLine.jsx
@@ -3,18 +3,36 @@ import MyButton from './MyButton'
 import { CloudIcon } from "@heroicons/react/solid";
 import { Teams } from './data';
 
-export default function TeamLine() {
-    return (
-        <div className=' h-full w-full flex justify-center'>
-            <div className='w-2/3  items-center flex justify-center'>
-                <CloudIcon className='w-8 h-8 ml-1 fill-green-600' />
-                <body className='text-black'>{Teams.at(0).team}</body>
-            </div>
-            <div className='w-1/3  flex justify-center'>
-                <MyButton />
-            </div>
+export default class TeamLine extends React.Component {
+    constructor(props) {
+        super(props);
+        this.test = Teams.at(1).team;
+        this.state = {
+            test: "test",
+            test2: "test",
+        };
+    };
 
-        </div>
-    )
+    ChangeText = () => {
+        this.setState({
+            test: "changed",
+            test2: "changed",
+        });
+    };
+    
+    render() {
+        return (
+            <div className=' h-full w-full flex justify-center'>
+                <div className='w-2/3  items-center flex justify-center'>
+                    <CloudIcon onClick={this.ChangeText} className='w-8 h-8 ml-1 fill-green-600' />
+                    <body className='text-black'>{this.state.test}</body>
+                </div>
+                <div className='w-1/3  flex justify-center'>
+                    <MyButton />
+                </div>
+
+            </div>
+        )
+    }
 }
 
