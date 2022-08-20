45355ba587932731341d019e5de8b2ec20374ee0
Cory <corylennox95@gmail.com>
2022-08-20 02:19:53 -0400

    update

--- src/components/TeamLine.jsx
+++ src/components/TeamLine.jsx
@@ -8,7 +8,7 @@ export default class TeamLine extends React.Component {
         return (
             <div className='border-2 border-yellow-600 h-full w-full flex justify-center'>
                 <div className='w-2/3  items-center flex justify-center'>
-                    <CloudIcon onClick={this.ChangeText} className='w-8 h-8 ml-1 fill-green-600' />
+                    <CloudIcon className='w-8 h-8 ml-1 fill-green-600' />
                     <body className='text-black'>{this.props.team}</body>
                 </div>
                 <div className='w-1/3  flex justify-center'>
