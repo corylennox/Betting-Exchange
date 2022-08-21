2c6d06143996e93ee18a7d0041d1ca2cc833d503
Cory <corylennox95@gmail.com>
2022-08-21 15:37:26 -0400

    changing outright bet structure to handle data different

--- src/components/OutrightBet.jsx
+++ src/components/OutrightBet.jsx
@@ -3,16 +3,20 @@ import { ContestantsData } from "../data";
 import ContestantRow from "./ContestantRow";
 
 export default class OutrightBet extends Component {
+  constructor(props) {
+    super(props);
+    this.contestantRow = ContestantsData.basketball.nbaTeams.map((nbaTeam) => (
+      <div class="mb-4 bg-gray-400 h-12">
+        <ContestantRow contestantData={nbaTeam} />
+      </div>
+    ));
+  }
   render() {
     return (
       <div className="h-full w-full border-2 border-red-600">
         <h2 className="text-white text-xl">NBA Championship</h2>
         <div class="grid grid-cols-3 mb-4 border-2 border-green-600">
-          {ContestantsData.basketball.nbaTeams.map((nbaTeam) => (
-            <div class="mb-4 bg-gray-400 h-12">
-              <ContestantRow contestantData={nbaTeam} />
-            </div>
-          ))}
+          {this.contestantRow}
         </div>
       </div>
     );
