388cba3099b8a4e1eb5c326e578f83c3b13860ee
Cory <corylennox95@gmail.com>
2022-08-27 02:49:35 -0400

    renamed file and formatting stuff

--- src/App.js
+++ src/App.js
@@ -1,39 +1,37 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import "./tailwind.css"
-import SportPane from './components/SportPane';
-import Sidebar from './components/Sidebar';
-import { BetData } from './betData'
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import React from "react";
+import Navbar from "./components/Navbar";
+import "./tailwind.css";
+import SportPane from "./components/SportPane";
+import Sidebar from "./components/Sidebar";
+import { BetData } from "./betData";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
     <main class="absolute inset-0 text-gray-400">
       <Router>
-      <Navbar />
-        <div className='h-full grid xs:grid-cols-1 lg:grid-cols-4'>
-          <div className=' bg-slate-900 border-t border-slate-100 flex justify-end h-full'>
+        <Navbar />
+        <div className="h-full grid xs:grid-cols-1 lg:grid-cols-4">
+          <div className=" bg-slate-900 border-t border-slate-100 flex justify-end h-full">
             <Sidebar betData={BetData} />
           </div>
           <div className="xs:col-span-1 lg:col-span-2 xl:w-auto w-full h-full">
             {
               <Routes>
-              <Route path="/">
-              {
-                BetData.map((betData) => (
-                  <Route path={betData.href} element={<SportPane betData={betData}/>}>
-                  </Route>
-                ))
-              }
-              </Route>
+                <Route path="/">
+                  {BetData.map((betData) => (
+                    <Route
+                      path={betData.href}
+                      element={<SportPane betData={betData} />}
+                    ></Route>
+                  ))}
+                </Route>
               </Routes>
             }
           </div>
-          <div className='bg-slate-50 shadow-xl drop-shadow-md h-full'><body>Betslip</body></div>
+          <div className="bg-slate-50 shadow-xl drop-shadow-md h-full">
+            <body>Betslip</body>
+          </div>
         </div>
       </Router>
     </main>
--- src/betData.js
+++ src/betData.js
@@ -1,142 +1,142 @@
-import { ContendersData } from "./contenders_data";
-import { svgs } from "./svgs"
+import { ContendersData } from "./contendersData";
+import { svgs } from "./svgs";
 
 export const BetData = [
-    {
-        sport: "Basketball",
-        href: 'basketball',
-        icon: svgs.basketball,
-        image: "https://cdn-icons-png.flaticon.com/512/921/921183.png",
-        availableBets: [
-            {
-                type: "outright",
-                title: "2022-23 NBA Championship Winner",
-                contendersData: ContendersData.basketball.nbaTeams,
-            },
-            {
-                type: "outright",
-                title: "2022-23 WNBA Championship Winner",
-                contendersData: ContendersData.basketball.wnbaTeams,
-            },
-            {
-                type: "game",
-                title: "Milwaukee Bucks @ Minnesota Timberwolves",
-                contender1Data: {
-                    contender: ContendersData.basketball.nbaTeams[0],
-                    spread: "+7.5",
-                    money: "+140",
-                    total: "O 160"
-                },
-                contender2Data: {
-                    contender: ContendersData.basketball.nbaTeams[1],
-                    spread: "-7",
-                    money: "-120",
-                    total: "U 160"
-                },
-            },
-            {
-                type: "game",
-                title: "Chicago Sky @ Minnesota Lynx",
-                contender1Data: {
-                    contender: ContendersData.basketball.wnbaTeams[0],
-                    spread: "+6.5",
-                    money: "+140",
-                    total: "O 160"
-                },
-                contender2Data: {
-                    contender: ContendersData.basketball.wnbaTeams[1],
-                    spread: "-7",
-                    money: "-120",
-                    total: "U 160"
-                },
-            },
-        ]
-    },
-    {
-        sport: "Baseball",
-        href: 'baseball',
-        icon: svgs.baseball,
-        image: "https://cdn-icons-png.flaticon.com/512/3371/3371359.png",
-        availableBets: [
-            {
-                type: "game",
-                title: "Chicago Cubs @ Seattle Mariners",
-                contender1Data: {
-                    contender: ContendersData.baseball.mlbTeams[0],
-                    spread: "-2.5",
-                    money: "-160",
-                    total: "O 16"
-                },
-                contender2Data: {
-                    contender: ContendersData.baseball.mlbTeams[1],
-                    spread: "+2",
-                    money: "+180",
-                    total: "U 16"
-                }
-            },
-            {
-                type: "game",
-                title: "Cincinnati Reds @ Philadelphia Phillies",
-                contender1Data: {
-                    contender: ContendersData.baseball.mlbTeams[2],
-                    spread: "-1.5",
-                    money: "-350",
-                    total: "O 6.5"
-                },
-                contender2Data: {
-                    contender: ContendersData.baseball.mlbTeams[3],
-                    spread: "+1",
-                    money: "+250",
-                    total: "U 6.5"
-                }
-            },
-            {
-                type: "game",
-                title: "Atlanta Braves @ Pittsburgh Pirates",
-                contender1Data: {
-                    contender: ContendersData.baseball.mlbTeams[4],
-                    spread: "-5",
-                    money: "-600",
-                    total: "O 8.5"
-                },
-                contender2Data: {
-                    contender: ContendersData.baseball.mlbTeams[5],
-                    spread: "+5.5",
-                    money: "+300",
-                    total: "U 8.5"
-                }
-            },
-        ]
-    },
-    {
-        sport: "Soccer",
-        href: 'soccer',
-        icon: svgs.soccer,
-        image: "https://cdn-icons-png.flaticon.com/512/907/907690.png",
-        availableBets: [
-            {
-                type: "outright",
-                title: "",
-                contendersData: ContendersData.basketball.nbaTeams,
-            },
-            {
-                type: "game",
-                title: "",
-                contender1Data: {
-                    name: "",
-                    image: "",
-                    spread: "",
-                    money: "",
-                    total: ""
-                },
-                contender2Data: {
-                    name: "",
-                    image: "",
-                    spread: "",
-                    money: "",
-                    total: ""
-                },
-            },
-        ],
-    }
+  {
+    sport: "Basketball",
+    href: "basketball",
+    icon: svgs.basketball,
+    image: "https://cdn-icons-png.flaticon.com/512/921/921183.png",
+    availableBets: [
+      {
+        type: "outright",
+        title: "2022-23 NBA Championship Winner",
+        contendersData: ContendersData.basketball.nbaTeams,
+      },
+      {
+        type: "outright",
+        title: "2022-23 WNBA Championship Winner",
+        contendersData: ContendersData.basketball.wnbaTeams,
+      },
+      {
+        type: "game",
+        title: "Milwaukee Bucks @ Minnesota Timberwolves",
+        contender1Data: {
+          contender: ContendersData.basketball.nbaTeams[0],
+          spread: "+7.5",
+          money: "+140",
+          total: "O 160",
+        },
+        contender2Data: {
+          contender: ContendersData.basketball.nbaTeams[1],
+          spread: "-7",
+          money: "-120",
+          total: "U 160",
+        },
+      },
+      {
+        type: "game",
+        title: "Chicago Sky @ Minnesota Lynx",
+        contender1Data: {
+          contender: ContendersData.basketball.wnbaTeams[0],
+          spread: "+6.5",
+          money: "+140",
+          total: "O 160",
+        },
+        contender2Data: {
+          contender: ContendersData.basketball.wnbaTeams[1],
+          spread: "-7",
+          money: "-120",
+          total: "U 160",
+        },
+      },
+    ],
+  },
+  {
+    sport: "Baseball",
+    href: "baseball",
+    icon: svgs.baseball,
+    image: "https://cdn-icons-png.flaticon.com/512/3371/3371359.png",
+    availableBets: [
+      {
+        type: "game",
+        title: "Chicago Cubs @ Seattle Mariners",
+        contender1Data: {
+          contender: ContendersData.baseball.mlbTeams[0],
+          spread: "-2.5",
+          money: "-160",
+          total: "O 16",
+        },
+        contender2Data: {
+          contender: ContendersData.baseball.mlbTeams[1],
+          spread: "+2",
+          money: "+180",
+          total: "U 16",
+        },
+      },
+      {
+        type: "game",
+        title: "Cincinnati Reds @ Philadelphia Phillies",
+        contender1Data: {
+          contender: ContendersData.baseball.mlbTeams[2],
+          spread: "-1.5",
+          money: "-350",
+          total: "O 6.5",
+        },
+        contender2Data: {
+          contender: ContendersData.baseball.mlbTeams[3],
+          spread: "+1",
+          money: "+250",
+          total: "U 6.5",
+        },
+      },
+      {
+        type: "game",
+        title: "Atlanta Braves @ Pittsburgh Pirates",
+        contender1Data: {
+          contender: ContendersData.baseball.mlbTeams[4],
+          spread: "-5",
+          money: "-600",
+          total: "O 8.5",
+        },
+        contender2Data: {
+          contender: ContendersData.baseball.mlbTeams[5],
+          spread: "+5.5",
+          money: "+300",
+          total: "U 8.5",
+        },
+      },
+    ],
+  },
+  {
+    sport: "Soccer",
+    href: "soccer",
+    icon: svgs.soccer,
+    image: "https://cdn-icons-png.flaticon.com/512/907/907690.png",
+    availableBets: [
+      {
+        type: "outright",
+        title: "",
+        contendersData: ContendersData.basketball.nbaTeams,
+      },
+      {
+        type: "game",
+        title: "",
+        contender1Data: {
+          name: "",
+          image: "",
+          spread: "",
+          money: "",
+          total: "",
+        },
+        contender2Data: {
+          name: "",
+          image: "",
+          spread: "",
+          money: "",
+          total: "",
+        },
+      },
+    ],
+  },
 ];
