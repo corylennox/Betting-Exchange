600b5e333fd50f7d0d0d388821ff5e580b335f75
Cory <corylennox95@gmail.com>
2022-08-26 22:37:37 -0400

    more css

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ export default function App() {
     <main class="absolute inset-0 text-gray-400">
       <Navbar />
  
-        <div className='h-full grid grid-cols-1 xl:grid-cols-4'>
+        <div className='h-full grid grid-cols-1 lg:grid-cols-4'>
           <div className=' bg-slate-900 border-t border-slate-100 flex justify-end h-full'>
             <body>sidebar</body>
           </div>
-          <div className="md:col-span-1 xl:col-span-2 xl:w-auto w-full h-full border-2 border-red-600">
+          <div className="md:col-span-1 lg:col-span-2 xl:w-auto w-full h-full">
             <SportPane />
           </div>
           <div className='bg-slate-50 shadow-xl drop-shadow-md h-full'><body>Betslip</body></div>
--- src/components/OutrightBet.jsx
+++ src/components/OutrightBet.jsx
@@ -33,7 +33,7 @@ export default class OutrightBet extends Component {
       <div className='bg-slate-100 rounded-2xl p-3 drop-shadow-md shadow-lg mb-3'>
         <h2 className="flex font-semibold text-blue-900 text-xl">{this.props.outrightBetData.title}</h2>
         <h3 className=" pb-2 font-semibold text-slate-900 text-md">Outright Bet</h3>
-        <div class="grid md:grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-2">
+        <div class="grid md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-2">
           {
             this.props.outrightBetData.contendersData.map((contenderData) => (
               <OutrightBetContenderRow contenderData={contenderData} />
