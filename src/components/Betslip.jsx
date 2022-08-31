f415a1eb6614d36bb83ed2192a29ec247de03ed7
Cory <corylennox95@gmail.com>
2022-08-31 13:17:10 -0400

    working on betslip

---
+++ src/components/Betslip.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Betslip extends Component {
+  render() {
+    return (
+      <div class="">
+          <h1>test</h1>
+      </div>
+    );
+  }
+}
