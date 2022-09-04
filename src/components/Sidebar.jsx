176a399d3d3f62721fd8bb81b46925fd58c2f1fc
Emerson Boyd <emerson.boyd@yahoo.com>
2022-09-04 15:00:36 -0500

    Make the entire sidebar zon clickable, add dividers for sportspane sidebars

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,14 +4,14 @@ import { ChevronRightIcon } from "@heroicons/react/outline";
 export default class Sidebar extends Component {
   render() {
     return (
-      <div class={this.props.sportsPane ? "pl-4 bg-white" : "pr-12"}>
-        <div class={this.props.sportsPane ? "mb-1 mt-3" : "mb-3 mt-3"}>
+      <div class={this.props.sportsPane ? "bg-white" : ""}>
+        <div class={this.props.sportsPane ? "mb-1 mt-3 ml-4" : "mb-3 mt-3 ml-5"}> {/* margin left to match the padding left in the <a/> tag */}
           <h1 className={this.props.sportsPane ? "text-gray-700 font-bold" : "text-slate-200 font-bold"}>All Sports</h1>
         </div>
         <div>
           {this.props.betData.map((sportData, index) => (
             <div className="contents">
-            <a class={this.props.sportsPane ? "flex justify-between items-center pb-4 pt-4 pl-1" : "flex justify-between items-center mb-3"} href={sportData.href}>
+            <a class={this.props.sportsPane ? "flex justify-between items-center pb-4 pt-4 pl-5" : "flex justify-between items-center pb-2 pt-2 pl-5 pr-12"} href={sportData.href}>
               <div className="flex items-center">
                 <img
                     className="w-5 h-5 invert mr-3"
@@ -24,7 +24,7 @@ export default class Sidebar extends Component {
                 <ChevronRightIcon className="w-4 h-4 mr-3 text-blue-500"/>
               </div>
             </a>
-            {(this.props.sportsPane && (index < this.props.betData.length - 1)) ? <div className="w-full h-px bg-gray-300"/> : <div/>}
+            {(this.props.sportsPane && (index < this.props.betData.length - 1)) ? <div className="w-full h-px bg-gray-300 ml-4"/> : <div/>}
             </div>
           ))}
         </div>
