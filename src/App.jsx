f445ea0d31b2a9fd3e2d688502fdb198fd886eb6
corylennox <corylennox95@gmail.com>
2022-09-06 23:46:18 -0400

    added graphQL and getdata

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,27 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import BottomNavbar from "./components/BottomNavbar";
 import Betslip from "./components/Betslip";
 //import MyButton from "./components/MyButton";
+import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink, from, } from "@apollo/client";
+import { onError } from '@apollo/client/link/error';
+import GetUsers from "./components/GetData";
+
+const errorLink = onError(({ graphqlErrors, networkError }) => {
+  if (graphqlErrors) {
+    graphqlErrors.map(({ message, location, path }) => {
+      alert(`Graphql error ${message}`);
+    });
+  }
+});
+
+const link = from([
+  errorLink,
+  new HttpLink({ uri: "http://70.241.98.220:4000/" })
+]);
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link,
+});
 
 export default class App extends Component {
   constructor(props) {
@@ -39,80 +60,83 @@ export default class App extends Component {
 
   render() {
     return (
-      <main className="absolute inset-0 w-full text-gray-400">
-        <Router>
-          {/* Navbar */}
-          <div className="hidden lg:contents">
-            <Navbar />
-          </div>
-          {/* Grid */}
-          <div className="grid xs:grid-cols-1 lg:grid-cols-6 min-h-screen">
-            {/* Sidebar */}
+      <ApolloProvider client={client}>
+        <main className="absolute inset-0 w-full text-gray-400">
+          <Router>
+            {/* Navbar */}
             <div className="hidden lg:contents">
-              <div className=" bg-slate-900 border-t border-slate-100 flex justify-end min-h-screen">
-                {this.sidebar}
-              </div>
+              <Navbar />
             </div>
-
-            {/* Sportspane */}
-            <div className="xs:col-span-1 lg:col-span-3 xl:w-auto w-full h-full min-h-screen">
-              {
-                <Routes>
-                  <Route path="/">
-                    {BetData.map((betData) => (
-                      <Route
-                        path={betData.href}
-                        element={
-                          <SportPane
-                            betData={betData}
-                            onMoneylineClick={this.onMoneylineClick}
-                          />
-                        }
-                      />
-                    ))}
-                  </Route>
-
-                  {/* All Sports in sports pane */}
-                  <Route
-                    path="/all-sports"
-                    element={
-                      <div>
-                        <Sidebar sportsPane={true} betData={BetData} />
-                      </div>
-                    }
-                  />
-
-                  {/* route all other paths to home */}
-                  <Route
-                    path="*"
-                    element={<SportPane betData={BetData[0]} />}
-                  />
-                </Routes>
-              }
-            </div>
-
-            {/* Betslip */}
-            <div className="hidden lg:block xs:col-span-1 lg:col-span-2 xl:auto w-full h-[calc(100vh-5rem)] sticky top-20 overflow-y-scroll overscroll-contain ">
-              <div className="h-auto ">
-                <div className="flex sticky top-0 border-b-2 h-11 items-center p-2">
-                  <div className="rounded-full bg-red-500 flex relative h-7 w-7 items-center text-center">
-                    <h1 className="w-full text-md font-semibold text-white font-mono">{this.state.activeBets.length}</h1>
-                  </div>
-                  <h1 className="text-slate-600 font-bold text-lg ml-1">Betslip</h1>
+            {/* Grid */}
+            <div className="grid xs:grid-cols-1 lg:grid-cols-6 min-h-screen">
+              {/* Sidebar */}
+              <div className="hidden lg:contents">
+                <div className=" bg-slate-900 border-t border-slate-100 flex justify-end min-h-screen">
+                  {this.sidebar}
                 </div>
-                <Betslip activeBets={this.state.activeBets} />
               </div>
-            </div>
 
-            {/* Bottom Navbar */}
-            <div className="contents lg:hidden 0">
-              <div className="inset-0 bottom-0 z-50 sticky bg-slate-100 h-16">
-                {this.BottomNavbar}
+              {/* Sportspane */}
+              <div className="xs:col-span-1 lg:col-span-3 xl:w-auto w-full h-full min-h-screen">
+                {
+                  <Routes>
+                    <Route path="/">
+                      {BetData.map((betData) => (
+                        <Route
+                          path={betData.href}
+                          element={
+                            <SportPane
+                              betData={betData}
+                              onMoneylineClick={this.onMoneylineClick}
+                            />
+                          }
+                        />
+                      ))}
+                    </Route>
+
+                    {/* All Sports in sports pane */}
+                    <Route
+                      path="/all-sports"
+                      element={
+                        <div>
+                          <Sidebar sportsPane={true} betData={BetData} />
+                        </div>
+                      }
+                    />
+
+                    {/* route all other paths to home */}
+                    <Route
+                      path="*"
+                      element={<SportPane betData={BetData[0]} />}
+                    />
+                  </Routes>
+                }
+              </div>
+
+              {/* Betslip */}
+              <div className="hidden lg:block xs:col-span-1 lg:col-span-2 xl:auto w-full h-[calc(100vh-5rem)] sticky top-20 overflow-y-scroll overscroll-contain ">
+                <div className="h-auto ">
+                  <div className="flex sticky top-0 border-b-2 h-11 items-center p-2">
+                    <div className="rounded-full bg-red-500 flex relative h-7 w-7 items-center text-center">
+                      <h1 className="w-full text-md font-semibold text-white font-mono">{this.state.activeBets.length}</h1>
+                    </div>
+                    <h1 className="text-slate-600 font-bold text-lg ml-1">Betslip</h1>
+                  </div>
+                  <GetUsers />
+                  <Betslip activeBets={this.state.activeBets} />
+                </div>
+              </div>
+
+              {/* Bottom Navbar */}
+              <div className="contents lg:hidden 0">
+                <div className="inset-0 bottom-0 z-50 sticky bg-slate-100 h-16">
+                  {this.BottomNavbar}
+                </div>
               </div>
             </div>
-          </div>
-        </Router>
-      </main>
+          </Router>
+        </main>
+      </ApolloProvider>
     );
   }
 }
