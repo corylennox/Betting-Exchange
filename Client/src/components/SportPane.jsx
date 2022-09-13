ce140372bd3e15d3199e597b75bf0e0551a2806f
Emerson Boyd <emerson.boyd@yahoo.com>
2022-09-12 20:16:21 -0500

    Turn the SportPane into a function

--- Client/src/components/SportPane.jsx
+++ Client/src/components/SportPane.jsx
@@ -2,50 +2,40 @@ import React, { Component } from "react";
 import OutrightBet from "./OutrightBet";
 import GameBet from "./GameBet";
 
-export default class SportPane extends Component {
-  constructor(props) {
-    super(props);
-
-    this.renderBets = this.renderBets.bind(this);
-  }
-
-  renderBets() {
-    return (this.props.betData.tabs.map((tab) => { return (
-      tab.availableBets.map((bet) => { 
-       switch (bet.type) {
-          case "OutrightBet":
-            return (
-              <div key={tab.tabTitle + bet.betTitle}>
-                <OutrightBet
-                  outrightBetData={bet}
-                  tabTitle={tab.tabTitle}
-                />
-              </div>
-            );
-          case "GameBet":
-            return (
-              <div key={tab.tabTitle + bet.betTitle}>
-                <GameBet
-                  gameBetData={bet}
-                  tabTitle={tab.tabTitle}
-                />
-              </div>
-            );
-          default:
-            return <></>;
-        }
-      })
-    )}));
-  }
-
-  render() {
+export default function SportPane(props) {
     return (
       <div className="h-full min-w-fit bg-slate-100 pl-8 pt-4 pr-8">
         <h1 className="mb-3 font-semibold font-sans text-2xl text-slate-900">
-          {this.props.betData.sportTitle}
+          {props.betData.sportTitle}
         </h1>
-        {this.renderBets()}
+        {
+          props.betData.tabs.map((tab) => { return (
+            tab.availableBets.map((bet) => { 
+             switch (bet.type) {
+                case "OutrightBet":
+                  return (
+                    <div key={tab.tabTitle + bet.betTitle}>
+                      <OutrightBet
+                        outrightBetData={bet}
+                        tabTitle={tab.tabTitle}
+                      />
+                    </div>
+                  );
+                case "GameBet":
+                  return (
+                    <div key={tab.tabTitle + bet.betTitle}>
+                      <GameBet
+                        gameBetData={bet}
+                        tabTitle={tab.tabTitle}
+                      />
+                    </div>
+                  );
+                default:
+                  return <></>;
+              }
+            })
+          )})
+        }
       </div>
     );
-  }
 }
