1be89095847ed3193a8009b019441bc60499c51c
Cory <corylennox95@gmail.com>
2022-10-07 19:14:42 -0400

    update

--- Client/src/components/ThemeButton.jsx
+++ Client/src/components/ThemeButton.jsx
@@ -5,15 +5,15 @@ import { changeThemeAction } from "../Actions";
 import { SunIcon, MoonIcon, DesktopComputerIcon } from "@heroicons/react/solid";
 
 const themeData = [
-  { name: "System", icon: <DesktopComputerIcon className="h-6 w-6" /> },
-  { name: "Light", icon: <SunIcon className="h-6 w-6" /> },
-  { name: "Dark", icon: <MoonIcon className="h-6 w-6" /> },
+  { id: 0, name: "System", icon: <DesktopComputerIcon className="h-6 w-6" /> },
+  { id: 1, name: "Light", icon: <SunIcon className="h-6 w-6" /> },
+  { id: 2, name: "Dark", icon: <MoonIcon className="h-6 w-6" /> },
 ];
 
 const themes = new Map();
-themes.set('System', themeData[0]);
-themes.set('Dark', themeData[1]);
-themes.set('Light', themeData[2]);
+themes.set("System", themeData[0]);
+themes.set("Dark", themeData[1]);
+themes.set("Light", themeData[2]);
 
 export default function ThemeButton() {
   //const [selectedTheme, setSelectedTheme] = useState(localStorage.theme === )
@@ -22,23 +22,22 @@ export default function ThemeButton() {
   return (
     <div className="relative h-full">
       <Listbox
-        value={activeTheme}
-        by="id"
-        onChange={useDispatch(changeThemeAction(activeTheme))}>
-        <Listbox.Button className="align-middle items-center h-full">
-          <span>
-            {themes.get(activeTheme).icon}
-          </span>
+        defaultValue={activeTheme}
+        onChange={useDispatch(changeThemeAction("Light"))}
+      >
+        <Listbox.Button className="h-full items-center align-middle">
+          <span>{themes.get(activeTheme).icon}</span>
         </Listbox.Button>
 
         {/* Menu Items */}
-        <Listbox.Options className="dark:highlight-white/5 h-96 absolute top-0 right-0 z-50 mt-8 w-36 overflow-hidden rounded-lg bg-white py-1 text-sm font-semibold text-slate-700 shadow-lg ring-1 ring-slate-900/10 dark:bg-slate-800 dark:text-slate-300 dark:ring-0">
-          {[...themes.keys()].map(theme => (
+        <Listbox.Options className="dark:highlight-white/5 absolute top-0 right-0 z-50 mt-8 h-96 w-36 overflow-hidden rounded-lg bg-white py-1 text-sm font-semibold text-slate-700 shadow-lg ring-1 ring-slate-900/10 dark:bg-slate-800 dark:text-slate-300 dark:ring-0">
+          {[...themes.keys()].map((theme) => (
             <Listbox.Option
-              key={theme.text}
+              key={theme.id}
               value={theme.name}
               className={({ active, selected }) =>
-                `cursor-pointer items-center px-2 py-1 ${active ? "bg-skin-accent" : ""
+                `cursor-pointer items-center px-2 py-1 ${
+                  active ? "bg-skin-accent" : ""
                 } ${selected ? "text-red-600" : ""}`
               }
             >
@@ -46,20 +45,8 @@ export default function ThemeButton() {
                 test
                 {theme.text}
               </span>
-
-
-
-
-
-
-
-
-
-
-
             </Listbox.Option>
           ))}
-
         </Listbox.Options>
       </Listbox>
     </div>
