2ce16e5c90ff65d1698fefc06e2d6e006849dd0c
Emerson Boyd <emerson.boyd@yahoo.com>
2022-10-24 00:50:47 -0500

    Persist user login throughout refreshes

--- Client/src/Auth0Wrapper.jsx
+++ Client/src/Auth0Wrapper.jsx
@@ -11,7 +11,11 @@ export default function Auth0Wrapper({ children }) {
         redirectUri={window.location.origin}
         audience={process.env.REACT_APP_AUDIENCE}
         scope="read:current_user update:current_user_metadata"
+        useRefreshTokens={true}
+        cacheLocation="memory"
     >
+        {/* useRefreshTokens={true} persists user login throughout refreshes */}
+        {/* cacheLocation="memory" persists user login throughout browser restarts. localStorage could work but should not be used: https://community.auth0.com/t/is-auth0-spa-js-storing-tokens-in-localstorage-vulnerable-to-xss/77145/5 */}
         { children }
     </Auth0Provider>
     );
