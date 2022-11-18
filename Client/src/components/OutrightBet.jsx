021d3c3da5a2a3b6496a9153c6f56da3542a3700
Emerson Boyd <emerson.boyd@yahoo.com>
2022-11-18 00:13:06 -0600

    Add dividers between OutrightBet columns

--- Client/src/components/OutrightBet.jsx
+++ Client/src/components/OutrightBet.jsx
@@ -45,9 +45,67 @@ export default class OutrightBet extends Component {
     this.showMoreCSS = this.showMoreCSS.bind(this);
   }
 
+  getCellClassName(index) {
+    return !this.state.isExpanded
+      ? index < numContenderRowsToDisplay
+        ? "contents"
+        : index < numContenderRowsToDisplay * 2
+        ? "hidden md:contents"
+        : index < numContenderRowsToDisplay * 3
+        ? "hidden xl:contents"
+        : "hidden"
+      : "" //expanded, so show all rows
+  }
+
+  /**
+   * @returns pt-0 if the cell is in the top row, pt-2 otherwise.
+   */
+  getCellPaddingTop(index) {
+    return index === 0 ? "pt-0" : index === 1 ? "pt-2 md:pt-0" : index === 2 ? "pt-2 xl:pt-0" : "pt-2"
+  }
+
+  /**
+   * @returns pb-0 if the cell is in the bottom row, pb-2 otherwise. The bottom row, in this case, refers
+   * to the bottom-most visible row. So, if the OutrightBet grid is not expanded, whatever the bottom-most
+   * visible row is will have pb-0.
+   */
+  getCellPaddingBottom(numContenders, index) {
+    const pbWhenXsAndNoShowMore = Math.floor((numContenders-1-index) / 1) > 0 ? "pb-2" : "pb-0";
+    const pbWhenMdAndNoShowMore = Math.floor((numContenders-1-index) / 2) > 0 ? "pb-2" : "pb-0";
+    const pbWhenXlAndNoShowMore = Math.floor((numContenders-1-index) / 3) > 0 ? "pb-2" : "pb-0";
+
+    const result = this.state.isExpanded
+      ?
+        index === numContenders-1 ? "pb-0" : index === numContenders-2 ? "pb-2 md:pb-0" : index === numContenders-3 ? "pb-2 xl:pb-0" : "pb-2"
+      :
+        index < numContenderRowsToDisplay-1 ? `${pbWhenXsAndNoShowMore} md:${pbWhenMdAndNoShowMore} xl:${pbWhenXlAndNoShowMore}` /* the case where there's a single column and it's not the bottom row */
+        : index < numContenderRowsToDisplay ? `pb-0 md:${pbWhenMdAndNoShowMore} xl:${pbWhenXlAndNoShowMore}` /* the case where there's a single column and it is the bottom row */
+        : index < numContenderRowsToDisplay*2-2 ? `pb-0 md:${pbWhenMdAndNoShowMore} xl:${pbWhenXlAndNoShowMore}` /* the case where there's two columns and it's not the bottom row */
+        : index < numContenderRowsToDisplay*2 ? `pb-0 xl:${pbWhenXlAndNoShowMore}` /* the case where there's two columns and it is the bottom row */
+        : index < numContenderRowsToDisplay*3-3 ? `pb-0 xl:${pbWhenXlAndNoShowMore}` /* the case where there's three columns and it's not the bottom row */
+        : "pb-0" /* the case where there's three columns and it is the bottom row */
+    return result;
+  }
+
+  getBorder(numContenders, index) {
+    const xsBorder = "border-r-0" /* no dividers in one-column grid */
+    const mdBorder = index < 2 && index === numContenders-1
+      ? "md:border-r-0" /* the case where this index is the final element in the first row - don't place a divider afterwards */
+      : (index+1) % 2 !== 0 ? "md:border-r-[1px]" : "md:border-r-0" /* the normal case for two-column grid */
+    const xlBorder = index < 3 && index === numContenders-1
+      ? "xl:border-r-0" /* the case where this index is the final element in the first row - don't place a divider afterwards */
+      : (index+1) % 3 !== 0 ? "xl:border-r-[1px]" : "xl:border-r-0" /* the normal case for three-column grid */
+
+    return `${xsBorder} ${mdBorder} ${xlBorder}`
+  }
+
   displayRows() {
+    const numContenders = this.props.outrightBetData.contendersData.length;
+
+    /* Add padding between cells rather than grid gap so the divider displays evenly between cells */
+    /* h-fit is needed in one of the inner divs so that if there are 2 columns and column 1 has 3 items, but column 2 has 2 items, the item in row 2 col 2 doesn't stretch to fit the padding created in row 2 col 1 */
     return (
-      <div className="grid xs:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+      <div className="grid xs:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-0">
         {this.props.outrightBetData.contendersData.map(
           (contenderData, index) => {
             return (
@@ -57,23 +115,18 @@ export default class OutrightBet extends Component {
                   this.props.outrightBetData.betTitle +
                   contenderData.name
                 }
-                className={
-                  !this.state.isExpanded
-                    ? index < numContenderRowsToDisplay
-                      ? "contents"
-                      : index < numContenderRowsToDisplay * 2
-                      ? "hidden md:contents"
-                      : index < numContenderRowsToDisplay * 3
-                      ? "hidden xl:contents"
-                      : "hidden"
-                    : "" //expanded, so show all rows
-                }
+                className={this.getCellClassName(index)}
               >
-                <OutrightBetContenderRow
-                  contenderData={contenderData}
-                  type={this.props.outrightBetData.type}
-                  title={this.props.outrightBetData.betTitle}
-                />
+                <div className={`border-skin-divier px-3 h-fit
+                  ${ this.getBorder(numContenders, index) }
+                  ${ this.getCellPaddingTop(index) }
+                  ${ this.getCellPaddingBottom(numContenders, index) }`}>
+                  <OutrightBetContenderRow
+                    contenderData={contenderData}
+                    type={this.props.outrightBetData.type}
+                    title={this.props.outrightBetData.betTitle}
+                  />
+                </div>
               </div>
             );
           }
