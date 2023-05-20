a7fe35e40a2307527bc13572daa1030497faacda
corylennox <corylennox95@gmail.com>
2023-05-20 17:50:19 -0400

    deposit button routes to account page

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { changeSportpaneAction, changeNavbarTabAction } from "../Actions";
 import ActiveThemeButton from "./ActiveThemeButton.jsx";
@@ -7,7 +7,7 @@ import rts from "../MyRoutes";
 import { useAuth0 } from "@auth0/auth0-react";
 import PromptButton from "./PromptButton";
 //import { UserCircleIcon } from "@heroicons/react/outline";
-import { ReactComponent as LogoSvgRaw } from '../images/logo.svg';
+import { ReactComponent as LogoSvgRaw } from "../images/logo.svg";
 import { convertToPriceString } from "./BetslipUtils";
 
 const navs = {
@@ -38,8 +38,15 @@ const loginItems = {
 export default function Navbar() {
   const dispatch = useDispatch();
   const activeSportPane = useSelector((state) => state.activeSportPane);
-  const { isAuthenticated, isLoading: isAuthLoading, loginWithRedirect, logout } = useAuth0();
+  const {
+    isAuthenticated,
+    isLoading: isAuthLoading,
+    loginWithRedirect,
+    logout,
+  } = useAuth0();
   const availableBalance = useSelector((state) => state.availableBalance);
+
+  const navigate = useNavigate();
 
   if (isAuthLoading) return <h1>Loading</h1>;
 
@@ -92,59 +99,67 @@ export default function Navbar() {
           </Link>
         </div>
         <div className="inline-flex h-full min-w-[22rem] max-w-[22rem] flex-nowrap items-center justify-end align-middle">
-          {
-            isAuthenticated
-              ?
-              <div className="contents">
-                <div className="text-right mr-2">
-                  <span className="block text-xs">Balance</span>
-                  <span className="block text-s">{`$${convertToPriceString(availableBalance)}`}</span>
-                </div>
-                <div className="flex h-full w-full items-center py-5 mr-4">
-                  <PromptButton onClick={() => { }} // onClick() should redirect to Strip deposit
-                    text="Deposit"
-                    textSize="text-m"
-                    gradientColorStart="from-skin-buttonAccentGradientStart"
-                    gradientColorEnd="to-skin-buttonAccentGradientEnd"
-                    gradientColorPressedStart="active:from-skin-buttonAccentPressed"
-                    gradientColorPressedEnd="active:to-skin-buttonAccentPressed" />
-                </div>
-                <div className="h-1/3 w-1 items-center bg-skin-sidebarDivider" />
-                <div className="h-8 w-8 mx-4">
-                  <Link
-                    to={loginItems.account.href}
-                    className="flex h-8 w-8 items-center"
-                    onClick={() => {
-                      dispatch(changeSportpaneAction(loginItems.account.href));
-                      dispatch(changeNavbarTabAction(rts.account));
-                    }}
-                  >
-                    <img className="h-8 w-8" src="accountIcon.svg" alt="logo" />
-                  </Link>
-                </div>
+          {isAuthenticated ? (
+            <div className="contents">
+              <div className="mr-2 text-right">
+                <span className="block text-xs">Balance</span>
+                <span className="text-s block">{`$${convertToPriceString(
+                  availableBalance
+                )}`}</span>
               </div>
-              :
-              <div className="contents">
-                <div className="flex h-full w-full items-center py-5 mr-4">
-                  <PromptButton onClick={() => loginWithRedirect({ screen_hint: 'login' })}
-                    text="Log in"
-                    textSize="text-m"
-                    gradientColorStart="from-skin-buttonAccentGradientStart"
-                    gradientColorEnd="to-skin-buttonAccentGradientEnd"
-                    gradientColorPressedStart="active:from-skin-buttonAccentPressed"
-                    gradientColorPressedEnd="active:to-skin-buttonAccentPressed" />
-                </div>
-                <div className="flex h-full w-full items-center py-5 mr-4">
-                  <PromptButton onClick={() => loginWithRedirect({ screen_hint: 'signup' })}
-                    text="Sign up"
-                    textSize="text-m"
-                    gradientColorStart="from-skin-buttonMutedGradientStart"
-                    gradientColorEnd="to-skin-buttonMutedGradientEnd"
-                    gradientColorPressedStart="active:from-skin-buttonMutedPressed"
-                    gradientColorPressedEnd="active:to-skin-buttonMutedPressed" />
-                </div>
+              <div className="mr-4 flex h-full w-full items-center py-5">
+                <PromptButton
+                  onClick={() => {
+                    navigate("/account");
+                  }} // onClick() should redirect to Strip deposit
+                  text="Deposit"
+                  textSize="text-m"
+                  gradientColorStart="from-skin-buttonAccentGradientStart"
+                  gradientColorEnd="to-skin-buttonAccentGradientEnd"
+                  gradientColorPressedStart="active:from-skin-buttonAccentPressed"
+                  gradientColorPressedEnd="active:to-skin-buttonAccentPressed"
+                />
               </div>
-          }
+              <div className="h-1/3 w-1 items-center bg-skin-sidebarDivider" />
+              <div className="mx-4 h-8 w-8">
+                <Link
+                  to={loginItems.account.href}
+                  className="flex h-8 w-8 items-center"
+                  onClick={() => {
+                    dispatch(changeSportpaneAction(loginItems.account.href));
+                    dispatch(changeNavbarTabAction(rts.account));
+                  }}
+                >
+                  <img className="h-8 w-8" src="accountIcon.svg" alt="logo" />
+                </Link>
+              </div>
+            </div>
+          ) : (
+            <div className="contents">
+              <div className="mr-4 flex h-full w-full items-center py-5">
+                <PromptButton
+                  onClick={() => loginWithRedirect({ screen_hint: "login" })}
+                  text="Log in"
+                  textSize="text-m"
+                  gradientColorStart="from-skin-buttonAccentGradientStart"
+                  gradientColorEnd="to-skin-buttonAccentGradientEnd"
+                  gradientColorPressedStart="active:from-skin-buttonAccentPressed"
+                  gradientColorPressedEnd="active:to-skin-buttonAccentPressed"
+                />
+              </div>
+              <div className="mr-4 flex h-full w-full items-center py-5">
+                <PromptButton
+                  onClick={() => loginWithRedirect({ screen_hint: "signup" })}
+                  text="Sign up"
+                  textSize="text-m"
+                  gradientColorStart="from-skin-buttonMutedGradientStart"
+                  gradientColorEnd="to-skin-buttonMutedGradientEnd"
+                  gradientColorPressedStart="active:from-skin-buttonMutedPressed"
+                  gradientColorPressedEnd="active:to-skin-buttonMutedPressed"
+                />
+              </div>
+            </div>
+          )}
           <div className="h-1/3 w-1 items-center bg-skin-sidebarDivider" />
           <span className="inline-block h-full items-center px-4 align-middle">
             <ActiveThemeButton />
