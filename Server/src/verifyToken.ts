559e36e7bde83264eb32ee7863301166a6439870
Emerson Boyd <emerson.boyd@yahoo.com>
2023-10-24 19:02:04 -0500

    Change AUDIENCE env variable to AUTH0_AUDIENCE

--- Server/src/verifyToken.ts
+++ Server/src/verifyToken.ts
@@ -11,7 +11,7 @@ export const verifyToken = async (bearerToken) => {
         jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
     });
 
-    //console.log(`Looking for bearer token ${bearerToken} from addr ${process.env.AUTH0_DOMAIN} and audience ${process.env.AUDIENCE}`);
+    //console.log(`Looking for bearer token ${bearerToken} from addr ${process.env.AUTH0_DOMAIN} and audience ${process.env.AUTH0_AUDIENCE}`);
     function getJwksClientKey(header, callback) {
         client.getSigningKey(header.kid, function (error, key: SigningKey | RsaSigningKey) {
             if (error)
@@ -26,7 +26,7 @@ export const verifyToken = async (bearerToken) => {
             bearerToken,
             getJwksClientKey,
             {
-                audience: process.env.AUDIENCE,
+                audience: process.env.AUTH0_AUDIENCE,
                 issuer: `https://${process.env.AUTH0_DOMAIN}/`,
                 algorithms: ["RS256"],
             },
