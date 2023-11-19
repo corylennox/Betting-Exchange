754cc4c9eb30426f65560e8a9ac7dd6aa5ae92f4
Emerson Boyd <emerson.boyd@yahoo.com>
2023-11-19 15:00:25 -0600

    Drop bet_title from game_bets table

---
+++ Common/db/migrations/20231119205812_init.js
@@ -0,0 +1,17 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return Promise.all([
+    knex.schema.alterTable("game_bets", (table) => {
+      table.dropColumn("bet_title");
+    }),
+  ]);
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {};
