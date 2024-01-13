e5b8f6daf63c94e8e7ccc5a5c519ae9bf71d43c3
Emerson Boyd <emerson.boyd@yahoo.com>
2024-01-12 21:57:19 -0600

    Make division in teams table not defaultable

---
+++ Common/db/migrations/20240113035504_init.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return Promise.all([
+    knex.schema.alterTable("teams", (table) => {
+      table.bigint("division_id").notNullable().alter();
+    }),
+  ]);
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return Promise.all([]);
+};
