f71081c0666eb15d5cfa28d7267e22b1084da71d
Emerson Boyd <emerson.boyd@yahoo.com>
2024-01-13 15:57:27 -0600

    Drop not applicable vendor_id column from leagues table

---
+++ Common/db/migrations/20240113215521_init.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return Promise.all([
+    knex.schema.alterTable("leagues", (table) => {
+      table.dropColumn("vendor_id");
+    }),
+  ]);
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return Promise.all([]);
+};
