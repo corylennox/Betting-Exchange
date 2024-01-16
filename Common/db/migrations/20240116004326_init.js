35f593cab08c3142055cbccd7cb4037bc2f0352c
Emerson Boyd <emerson.boyd@yahoo.com>
2024-01-15 18:48:22 -0600

    Add league_id to individuals table

---
+++ Common/db/migrations/20240116004326_init.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return Promise.all([
+    knex.schema.alterTable("individuals", (table) => {
+      table.bigint("league_id").notNullable();
+    }),
+  ]);
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return Promise.all([]);
+};
