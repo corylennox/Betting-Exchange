0f83405ca4795d1da129c09481d87348a5246746
Emerson Boyd <emerson.boyd@yahoo.com>
2024-02-06 20:22:16 -0600

    Add more information to the outright_bets table

---
+++ Common/db/migrations/20240207021633_init.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return Promise.all([
+    knex.schema.alterTable("outright_bets", (table) => {
+      table.bigint("league_id").notNullable();
+      table.string("symbol").notNullable();
+    }),
+  ]);
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return Promise.all([]);
+};
